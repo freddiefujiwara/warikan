f1849c8ab9b4dea0eb1aea27ee3e34777888aad4
Fumikazu Fujiwara | Freddie <github@ze.gs>
2023-07-08 22:30:16 +0000

    add test

--- src/CalculationPage.test.js
+++ src/CalculationPage.test.js
@@ -10,16 +10,16 @@ test('renders form and performs calculation', async () => {
     const totalCostInput = getByLabelText(/Total cost:/i);
     const percentageInput = getByLabelText(/Percentage of the cost your side will cover:/i);
 
-    fireEvent.change(peopleOnYourSideInput, { target: { value: '2' } });
-    fireEvent.change(peopleOnOtherSideInput, { target: { value: '2' } });
-    fireEvent.change(totalCostInput, { target: { value: '200' } });
+    fireEvent.change(peopleOnYourSideInput, { target: { value: '5' } });
+    fireEvent.change(peopleOnOtherSideInput, { target: { value: '5' } });
+    fireEvent.change(totalCostInput, { target: { value: '50000' } });
     fireEvent.change(percentageInput, { target: { value: '50' } });
 
     fireEvent.click(getByText(/Calculate/i));
 
     await waitFor(() => {
-        expect(getByText(/Your side total: 100/i)).toBeInTheDocument();
-        expect(getByText(/Other side total: 0/i)).toBeInTheDocument();
+        expect(getByText(/Your side total: 5000/i)).toBeInTheDocument();
+        expect(getByText(/Other side total: 5000/i)).toBeInTheDocument();
         expect(getByText(/Change: 0/i)).toBeInTheDocument();
     });
 
