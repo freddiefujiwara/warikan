0cd971a55f7748f397ad056484d3fb86f3afe723
Fumikazu Fujiwara | Freddie <github@ze.gs>
2023-07-08 21:51:05 +0000

    add Calculation Page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders CalculationPage component', () => {
+    const { getByLabelText } = render(<App />);
+
+    const peopleOnYourSideInput = getByLabelText(/Number of people on your side:/i);
+    const peopleOnOtherSideInput = getByLabelText(/Number of people on the other side:/i);
+    const totalCostInput = getByLabelText(/Total cost:/i);
+    const percentageInput = getByLabelText(/Percentage of the cost your side will cover:/i);
+
+    expect(peopleOnYourSideInput).toBeInTheDocument();
+    expect(peopleOnOtherSideInput).toBeInTheDocument();
+    expect(totalCostInput).toBeInTheDocument();
+    expect(percentageInput).toBeInTheDocument();
 });
